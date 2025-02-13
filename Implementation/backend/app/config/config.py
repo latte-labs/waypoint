b87acdc1891ca81c72ba6c3731a5553bbb839fe2
Russell <russellhanjosef@gmail.com>
2025-02-12 16:48:48 -0800

    Update config.py
    To add GOOGLE_PLACES_API_KEY

--- Implementation/backend/app/config/config.py
+++ Implementation/backend/app/config/config.py
@@ -6,5 +6,6 @@ load_dotenv()
 class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./test.db")
     SECRET_KEY: str = os.getenv("SECRET_KEY", "your_secret_key_here")
+    GOOGLE_PLACES_API_KEY: str = os.getenv("GOOGLE_PLACES_API_KEY")
 
 settings = Settings()
